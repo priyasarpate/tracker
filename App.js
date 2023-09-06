4d2a27efa1ed4b5f9541620c84134249a719f604
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-09-06 23:17:13 +0530

    auth

--- App.js
+++ App.js
@@ -2,14 +2,15 @@
 import Email from "./auth/Email";
 import MobileNumber from "./auth/MobileNumber";
 import VerifyNumber from "./auth/VerifyNumber";
-
+import DOB from "./components/DOB";
 
 export default function App() {
   return (
     <>
     {/* <MobileNumber /> */}
-    <VerifyNumber />
+    {/* <VerifyNumber /> */}
       {/* <Email /> */}
+      <DOB />
     </>
   );
 }
---
+++ components/DOB.js
@@ -0,0 +1,21 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+export default function DOB() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>
+            add date of birth
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
